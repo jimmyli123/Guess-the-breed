25f25fc57b15c202155b5208354ea83e59a4d099
Jimmy Li <lijjimmy@gmail.com>
2021-07-20 23:55:14 -0400

    implemented the game logic, scoreboard, and added styling to the page

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,55 @@
-document.querySelector('#takeTheGuess').addEventListener('click', justDoIt)
+document.querySelector('#takeTheGuess').addEventListener('click', checkAnswer)
 document.querySelector('#begin').addEventListener('click', playTime)
 let dogUrl = 'https://dog.ceo/api/breeds/image/random'
 
+let currentBreed;
 
 function playTime() {
     document.querySelector('#gameTime').style.display = "block"
+    justDoIt()
+}
+
+let totalCounter = 0;
+let correctGuesses = 0;
+
+
+function justDoIt() {
+    document.querySelector('#begin').style.display = "none"
     fetch(dogUrl)
     .then(res=> res.json())
     .then(data => { 
         console.log(data)
         document.querySelector('img').src=data.message;
-        let breed = data.message.split('/')
-        console.log(`Breed= ${breed}`)
-        
+        currentBreed = getBreedFromString(data.message)
+        console.log(currentBreed)
     })
-}
-
-let counter = 0;
-
-
-
-function justDoIt() {
-    document.getElementById('totalQuestions').innerText = counter+1;
-    counter++;
+    
     
 }
+
+function checkAnswer() {
+    let guess = document.querySelector('#guess').value
+    if (guess.toLowerCase() === currentBreed) {
+        correctGuesses++
+        document.getElementById('result').innerText = 'Wow, you are correct!'
+    }
+    else {
+        document.getElementById('result').innerText = 'Unfortunately, you are wrong.'
+    }
+    document.querySelector('#playerScore').innerText = correctGuesses;
+    document.getElementById('totalQuestions').innerText = totalCounter+1;
+    totalCounter++;
+    justDoIt()
+}
+
+function getBreedFromString(word) {
+    let needFilter = word.split('/')
+    let indexOfBreed = 0;
+    for (let i =0; i <needFilter.length; i++) {
+        if (needFilter[i] === 'breeds'){
+            indexOfBreed = i+1
+        }
+    }
+    return needFilter[indexOfBreed].split('-').join(' ')
+}
+
